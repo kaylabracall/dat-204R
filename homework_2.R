8f942ac27d24fd102488e1de2803b6c3545751a8
Kayla Bracall <kayla.bracall@acd.ccac.edu>
2020-09-23 22:00:49 -0400

    homeowork 2

--- homework_2.R
+++ homework_2.R
@@ -6,21 +6,31 @@ library('ggplot2')
 ?mpg
 ?qplot
 
+#mpg
+
 # find city mpg of manual vs automatic transmissions
-qplot(trans, cty, data = mpg, geom = "point")
+qplot(trans, cty, data = mpg, geom = "boxplot", fill = trans, 
+      main = "City MPG by Transmission Type", ylab = "City MPG", 
+      xlab = "Transmission Type")
 
 # find highway mpg of manual vs automatic transmissions
-qplot(trans, hwy, data = mpg)
+qplot(trans, hwy, data = mpg, geom = "boxplot", fill = trans,
+      main = "Highway MPG by Transmission Type", ylab = "Highway MPG", 
+      xlab = "Transmission Type")
 
 # find mpg by year
-qplot(year, cty, data = mpg)
+#qplot(year, cty, data = mpg)
 
 # find city mpg by model
-qplot(model, cty, data = mpg)
+#qplot(model, cty, data = mpg)
 
-# find city mpg by cary type
-qplot(class, cty, data = mpg) + geom_boxplot(fill = '#A4A4A4')
+# find city mpg by car type
+qplot(class, cty, data = mpg, geom = "boxplot", fill = class, 
+      main = "City MPG by Car Type", ylab = "City MPG", xlab = "Car Type")
 
-# find highway mpg by cary type
-qplot(class, hwy, data = mpg, geom = "boxplot")
+# find highway mpg by car type
+qplot(class, hwy, data = mpg, geom = "boxplot", fill = class, 
+      main = "Highway MPG by Car Type", ylab = "Highway MPG", xlab = "Car Type")
+
+
 
